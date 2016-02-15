c16e7476f5dc6c5e573d627ad135399af60849d3
annais <annaisjh92@hotmail.com>
2016-02-15 08:37:26 +0100

    portfolio

--- js/menu.js
+++ js/menu.js
@@ -5,4 +5,41 @@ jQuery(document).ready(function() {
     });
 
     jQuery('.letter').toggle('explode');
+
+    $('#entrada').height($(window).height());
+    $('#entrada').resize(function(){
+    	$('#entrada').height($(window).height());
+    });
+
+    $('#sobremi').click(function(e){
+    	e.preventDefault();
+    	$('html, body').animate({scrollTop: $('#datos').offset().top}, 800);
+    });
+        
+    $('#projectos').click(function(e){
+    	e.preventDefault();
+    	$('html, body').animate({scrollTop: $('#leng').offset().top}, 800);
+    });
+
+    $('#contactar').click(function(e){
+    	e.preventDefault();
+    	$('html, body').animate({scrollTop: $('#contacto').offset().top}, 800);
+    });
+
+    $('#leng').waypoint(function() {
+      $('.nleng').toggle('slide');
+    },{
+        offset: '10%',
+        triggerOnce:true
+    });
+
+    $('#datos').waypoint(function() {
+      $('.imgdatos').toggle('slide');
+    },{
+        offset: '10%',
+        triggerOnce:true
+    });
+
+
 });
+
